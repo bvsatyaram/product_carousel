6d12235eb7e789d070b81b8af41ed85027862972
BV Satyaram <bvsatyaram@gmail.com>
2017-06-26 02:27:19 +0530

    Scope admin routes within a namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get '/carousel/preview', to: 'carousel#preview', as: 'preview'
-  get '/carousel/configure', to: 'carousel#configure', as: 'configure'
+  namespace :admin do
+    get '/carousel/preview', to: 'carousel#preview', as: 'preview'
+    get '/carousel/configure', to: 'carousel#configure', as: 'configure'
 
-  resources :shopify_products, only: [:index]
-  resources :carousel_items, only: [:index, :create, :destroy]
-  root :to => 'carousel#preview'
+    resources :shopify_products, only: [:index]
+    resources :carousel_items, only: [:index, :create, :destroy]
+  end
+
+  root :to => 'admin/carousel#preview'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
