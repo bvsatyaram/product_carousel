213a40ea0271659b95ae0cb2c96161ef9f0452dd
BV Satyaram <bvsatyaram@gmail.com>
2017-06-26 06:49:45 +0530

    Update preview page with the carousel

---
+++ app/assets/javascripts/carousel-mounter.js
@@ -0,0 +1,153 @@
+(function(){
+  var loadScript = function(url, callback){
+    // Source: http://snipplr.com/view/18756/loadscript/
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState){  //IE
+      script.onreadystatechange = function(){
+        if (script.readyState == "loaded" || script.readyState == "complete") {
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    } else {  //Others
+      script.onload = function(){
+        callback();
+      };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+  };
+
+  var Carousel = {
+    selector: '.toptal-product-slider',
+    proxyUri: '/slides/',
+    loadCarouselStyles: function() {
+      $('<link>').attr({
+        rel:  "stylesheet",
+        type: "text/css",
+        href: "//b5b4c70a.ngrok.io/script-tags/assets/css/carousel-styler.css"
+      }).appendTo($('body'));
+    },
+    getSlides: function() {
+      var shopify_domain = "http://" + $('.toptal-product-slider')[0].dataset.shopifyDomain;
+      $.get(
+        this.proxyUri,
+        {shopify_domain: shopify_domain},
+        this.populateSlides
+      )
+    },
+    populateSlides: function(response) {
+      var sliderDiv = $('<div>').addClass('slider');
+      var sliderWrapper = $('<div>').addClass('slider-wrapper');
+      var sliderNav = $('<div>').addClass('slider-nav');
+      var sliderIndex = 0;
+      response.slides.forEach(function(slide) {
+        var slideDiv = $('<div>').addClass('slide');
+        var infoDiv = $('<div>').addClass('slide-info');
+        infoDiv.append(
+          $('<p>').addClass('product-title').html(slide.title)
+        ).append(
+          $('<p>').addClass('product-type').html(slide.product_type)
+        ).append(
+          $('<a>').attr('href', '/products/' + slide.handle).html('Buy Now')
+        );
+        slideDiv.append(
+          $('<img>').attr('src', slide.image)
+        ).append(infoDiv).append(
+          $('<p>').addClass('caption').html(slide.title)
+        );
+        slideDiv.appendTo(sliderWrapper);
+
+        var navLink = $('<a href="#" data-slide="' + String(sliderIndex) + '">' + String(sliderIndex + 1) + '</a>');
+        navLink.appendTo(sliderNav);
+
+        sliderIndex ++;
+      });
+      sliderWrapper.appendTo(sliderDiv);
+      sliderNav.appendTo(sliderDiv);
+
+      $('.toptal-product-slider').html(sliderDiv);
+
+      function Slider( element ) {
+      	this.el = document.querySelector( element );
+      	this.init();
+      }
+
+      Slider.prototype = {
+      	init: function() {
+      		this.links = this.el.querySelectorAll( ".slider-nav a" );
+      		this.wrapper = this.el.querySelector( ".slider-wrapper" );
+      		this.navigate();
+      	},
+      	navigate: function() {
+
+      		for( var i = 0; i < this.links.length; ++i ) {
+      			var link = this.links[i];
+      			this.slide( link );
+      		}
+      	},
+
+      	animate: function( slide ) {
+      		var parent = slide.parentNode;
+      		var caption = slide.querySelector( ".caption" );
+      		var captions = parent.querySelectorAll( ".caption" );
+      		for( var k = 0; k < captions.length; ++k ) {
+      			var cap = captions[k];
+      			if( cap !== caption ) {
+      				cap.classList.remove( "visible" );
+      			}
+      		}
+      		caption.classList.add( "visible" );
+      	},
+
+      	slide: function( element ) {
+      		var self = this;
+      		element.addEventListener( "click", function( e ) {
+      			e.preventDefault();
+      			var a = this;
+      			self.setCurrentLink( a );
+      			var index = parseInt( a.getAttribute( "data-slide" ), 10 ) + 1;
+      			var currentSlide = self.el.querySelector( ".slide:nth-child(" + index + ")" );
+
+      			self.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
+      			self.animate( currentSlide );
+
+      		}, false);
+      	},
+      	setCurrentLink: function( link ) {
+      		var parent = link.parentNode;
+      		var a = parent.querySelectorAll( "a" );
+
+      		link.className = "current";
+
+      		for( var j = 0; j < a.length; ++j ) {
+      			var cur = a[j];
+      			if( cur !== link ) {
+      				cur.className = "";
+      			}
+      		}
+      	}
+      };
+
+      new Slider('.toptal-product-slider .slider');
+    },
+    init: function() {
+      $(this.selector).html('<p>Loading Carousel...</p>');
+      this.loadCarouselStyles();
+      this.getSlides();
+    }
+  }
+
+  var carouselMounter = function($){
+    if ($('.toptal-product-slider').length > 0) {
+      Carousel.init();
+    }
+  };
+
+  $(document).ready(function() {
+    carouselMounter($);
+  });
+})();
