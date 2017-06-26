edc67f2d4f14b84e4fea318397e896842a49df57
BV Satyaram <bvsatyaram@gmail.com>
2017-06-26 05:58:11 +0530

    Add slides routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :carousel_items, only: [:index, :create, :destroy]
   end
 
+  resources :slides, only: [:index]
+
   root :to => 'admin/carousel#preview'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
